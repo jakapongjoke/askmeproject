1b6211e4f7bc6cecb9f419fc8ab27e6d0ae4488f
jakapongjoke <jakapong991@hotmail.com>
2018-10-26 23:29:21 +0700

    test 1 command comit changes

--- wp-content/themes/wp-bootstrap-4/header.php
+++ wp-content/themes/wp-bootstrap-4/header.php
@@ -25,22 +25,7 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp-bootstrap-4' ); ?></a>
 
 	<header id="masthead" class="site-header <?php if ( get_theme_mod( 'sticky_header', 0 ) ) : echo 'sticky-top'; endif; ?>">
-	
 		<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-dark bg-dark">
-		<?php
-					wp_nav_menu( array(
-						'theme_location'  => 'menu-1',
-						'menu_id'         => 'primary-menu',
-						'container'       => 'div',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'primary-menu-wrap',
-						'menu_class'      => 'navbar-nav ml-auto',
-			            'fallback_cb'     => '__return_false',
-			            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-			            'depth'           => 2,
-			            'walker'          => new WP_bootstrap_4_walker_nav_menu()
-					) );
-				?>
 			<?php if( get_theme_mod( 'header_within_container', 0 ) ) : ?><div class="container"><?php endif; ?>
 				<?php the_custom_logo(); ?>
 
@@ -66,7 +51,20 @@
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#primary-menu-wrap" aria-controls="primary-menu-wrap" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-			
+				<?php
+					wp_nav_menu( array(
+						'theme_location'  => 'menu-1',
+						'menu_id'         => 'primary-menu',
+						'container'       => 'div',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'primary-menu-wrap',
+						'menu_class'      => 'navbar-nav ml-auto',
+			            'fallback_cb'     => '__return_false',
+			            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+			            'depth'           => 2,
+			            'walker'          => new WP_bootstrap_4_walker_nav_menu()
+					) );
+				?>
 			<?php if( get_theme_mod( 'header_within_container', 0 ) ) : ?></div><!-- /.container --><?php endif; ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
