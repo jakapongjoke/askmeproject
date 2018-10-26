e6032d8b7bda2eb3274d3c318c05748f1806264d
jakapongjoke <jakapong991@hotmail.com>
2018-10-26 23:27:44 +0700

    test 1 command comit changes

--- wp-content/themes/wp-bootstrap-4/header.php
+++ wp-content/themes/wp-bootstrap-4/header.php
@@ -23,7 +23,11 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp-bootstrap-4' ); ?></a>
-	<?php
+
+	<header id="masthead" class="site-header <?php if ( get_theme_mod( 'sticky_header', 0 ) ) : echo 'sticky-top'; endif; ?>">
+	
+		<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-dark bg-dark">
+		<?php
 					wp_nav_menu( array(
 						'theme_location'  => 'menu-1',
 						'menu_id'         => 'primary-menu',
@@ -37,10 +41,6 @@
 			            'walker'          => new WP_bootstrap_4_walker_nav_menu()
 					) );
 				?>
-			<?php if( get_theme_mod( 'header_within_container', 0 ) ) : ?></div><!-- /.container --><?php endif; ?>
-	<header id="masthead" class="site-header <?php if ( get_theme_mod( 'sticky_header', 0 ) ) : echo 'sticky-top'; endif; ?>">
-
-		<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-dark bg-dark">
 			<?php if( get_theme_mod( 'header_within_container', 0 ) ) : ?><div class="container"><?php endif; ?>
 				<?php the_custom_logo(); ?>
 
@@ -66,7 +66,8 @@
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#primary-menu-wrap" aria-controls="primary-menu-wrap" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-
+			
+			<?php if( get_theme_mod( 'header_within_container', 0 ) ) : ?></div><!-- /.container --><?php endif; ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
